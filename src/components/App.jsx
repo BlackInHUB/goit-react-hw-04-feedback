9f9ba8ee9f467fca79e2e2ed2ca4181f01f47107
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-10-04 11:34:09 +0300

    Refacted to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,39 @@
-import React from "react";
+import {useState} from "react";
 import { Section } from "./Feedback/Section/Section";
 import { FeedbackOptions } from './Feedback/FeedbackOptions/FeedbackOptions'
 import { Statistics } from './Feedback/Statistics/Statistics'
 import { Notification } from './Feedback/Notification/Notification'
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  }
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  handleBtnClick = (event) => {
+  const handleBtnClick = (event) => {
     const tartgetBtn = event.target.name;
     
-    this.setState(prevState => ({
-      [tartgetBtn]: prevState[tartgetBtn] + 1
-    }))
+    switch (tartgetBtn) {
+      case 'good':
+        setGood(state => state + 1)
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1)
+        break;
+      case 'bad':
+        setBad(state => state + 1)
+        break;
+      default:
+        return;
+    }
   }
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positivePercentage = Number((((good + neutral) / total) * 100).toFixed());
+  const total = good + neutral + bad;
+  const positivePercentage = Number((((good + neutral) / total) * 100).toFixed());
 
-    return (
-      <Section title="Please, leave your feedback">
-        <FeedbackOptions options={this.state} onLeaveFeedback={this.handleBtnClick} />
-        {total > 0 ? <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage}/> : <Notification />}
-      </Section>
-    )
-  }
+  return (
+    <Section title="Please, leave your feedback">
+      <FeedbackOptions options={{good, neutral, bad}} onLeaveFeedback={handleBtnClick} />
+      {total > 0 ? <Statistics good={good} neutral={neutral} bad={bad} total={total} positivePercentage={positivePercentage}/> : <Notification />}
+    </Section>
+  )
 };
-
-export default App;
